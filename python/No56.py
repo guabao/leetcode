90f4d5ee64d2a3bd28677ba7a701404af37501fc
guabao <wjz270025872@gmail.com>
2017-05-18 06:47:35 -0400

    Update No56.py

--- python/No56.py
+++ python/No56.py
@@ -1,10 +1,20 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
 class Solution(object):
-    def spiralOrder(self, matrix):
+    def merge(self, intervals):
         """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
         """
-        #if not matrix:
-        #    return []
-        #else:
-        return matrix and list(matrix[0]) + self.spiralOrder(zip(*matrix[1:])[::-1])
+        intervals = sorted(intervals, key = operator.attrgetter('start'))
+        l = [intervals[0]] if intervals else []
+        for i in xrange(1, len(intervals)):
+            if intervals[i].start <= l[-1].end:
+                l[-1].end = max(intervals[i].end, l[-1].end)
+            else:
+                l += [intervals[i]]
+        return l
