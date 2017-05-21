94ad8d9943de6a8a49838cf80751b53e9a4d9d8a
guabao <wjz270025872@gmail.com>
2017-05-21 10:24:34 -0400

    Update and rename No104.py to No107.py

---
+++ python/No107.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        l = [root]
+        values = []
+        i = 0
+        while l:
+            l_new = []
+            values_new = []
+            for li in l:
+                values_new.append(li.val)
+                if li.left:
+                    l_new.append(li.left)
+                if li.right:
+                    l_new.append(li.right)
+            values.append(values_new)
+            l = l_new
+            
+        return values[::-1]
